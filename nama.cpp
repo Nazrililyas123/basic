dad4ab7043c55e692d07861fa45017989cd7d05d
nazril <111737041+Nazrililyas123@users.noreply.github.com>
2022-11-28 14:24:36 +0700

    Add files via upload

---
+++ nama.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout <<"Nama:Nazril ilyas robiansyah!"<<endl;
+
+    cout <<"kelas: X rpl 2"<<endl;
+    
+    cout <<"nilai: 85"<<endl;
+    return 0;
+}
